a265e50ef9c3dbd59f6d5962eb107ad8d711a9d5
Jordan Gillard <jordan-gillard@outlook.com>
2018-05-20 03:52:55 +0000

    Pretty much done. Add tests?

--- main.py
+++ main.py
@@ -46,32 +46,58 @@ def check_csv_file_is_good(file):
             if len(row) != 2 and len(row) != 0:
                 print(('This program will skip row: "' + "{},"*len(row) + '" because it is improperly formatted. Rows must be in the format "menu item, price".').format(*row))
     return True
-        
+    
+def get_target_price(header):
+    target_price = header[1].strip()
+    target_price_no_currency_sign = target_price[1:]
+    target_price_string = "${:.02f}".format(float(target_price_no_currency_sign))
+    return target_price_string
+    
+def check_if_row_is_good(row):
+    if not row: #skips empty rows 
+        return False
+    elif len(row) != 2: #skips improperly formatted rows.
+        return False
+    return True
+    
+def check_if_there_are_combos_that_match_price(combinations, target_price, foods):
+    if combinations:
+        for combination in combinations:
+            message_to_user = ""
+            for menu_item in combination:
+                item = menu_item[0]
+                price = menu_item[1]
+                dish_name = foods[item]
+                message_to_user += '{} at ${:.02f}'.format(dish_name, price)
+                if len(combination) != 1:
+                    message_to_user += ', '
+                else:
+                    message_to_user += ' '
+            message_to_user += "is equal to the target price of {}.".format(target_price_string)
+            print(message_to_user)
+            return
+    print("There are no combinations of menu items that match the target price.")
+    
 for file in args.csv:
     if check_csv_file_is_good(file):
         with open(file, 'r') as f:
             reader = csv.reader(f)
             target_price_row = next(reader)
-            target_price = target_price_row[1].strip()
-            target_price_no_currency_sign = target_price[1:]
-            target_price_string = "${:.02f}".format(float(target_price_no_currency_sign))
+            target_price_string = get_target_price(target_price_row)
             foods = []
             prices = []
+            
             for row in reader:
-                if not row: #skips empty rows 
-                    pass
-                elif len(row) != 2: #skips improperly formatted rows.
-                    pass
-                    # print(('skipped row: "' + "{},"*len(row) + '" because it is improperly formatted.').format(*row))
-                else:
+                if check_if_row_is_good(row):
                     food = row[0]
-                    price = float(row[1].replace('$', ''))
+                    price = float(row[1].replace('$', '')) # $4 => 4.00
                     foods.append(food)
                     prices.append(price)
-            enumerated_foods_list = list(enumerate(foods))
+                    
             enumerated_prices_list = list(enumerate(prices))
-            all_possible_combinations_of_prices = [combo for i in range(len(enumerated_foods_list), 0, -1) for combo in itertools.combinations(enumerated_prices_list, i)]
+            all_possible_combinations_of_prices = [combo for i in range(len(enumerated_prices_list), 0, -1) for combo in itertools.combinations(enumerated_prices_list, i)]
             combinations_that_match_target_price = []
+            
             for combination in all_possible_combinations_of_prices:
                 total_price = 0
                 for price in combination:
@@ -79,19 +105,6 @@ for file in args.csv:
                 total_price_string = "${0:.2f}".format(total_price)
                 if total_price_string == target_price_string:
                     combinations_that_match_target_price.append(combination)
-            for combination in combinations_that_match_target_price:
-                message_to_user = ""
-                for menu_item in combination:
-                    item = menu_item[0]
-                    price = menu_item[1]
-                    dish_name = foods[item]
-                    message_to_user += '{} at ${:.02f}'.format(dish_name, price)
-                    if len(combination) != 1:
-                        message_to_user += ', '
-                    else:
-                        message_to_user += ' '
-                message_to_user += "is equal to the target price of {}.".format(target_price_string)
-                
-                print(message_to_user)
-            
+                    
+            check_if_there_are_combos_that_match_price(combinations_that_match_target_price, target_price_string, foods)
         
